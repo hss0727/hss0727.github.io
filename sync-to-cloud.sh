0489d1abe3f087dbb4d6926115a63d025e4592d9
hss0727 <hyuks0727@gmail.com>
2022-09-08 09:40:48 -0400

    GITHUB-UPLOAD:DATE-2022-09-08-TIME-09-40-48

---
+++ sync-to-cloud.sh
@@ -0,0 +1,8 @@
+rsync -alvr --delete portfolio hyuksoog@gtown3.reclaimhosting.com:/home/hyuksoog/public_html/
+
+DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
+message="GITHUB-UPLOAD:$DATE";
+echo "commit message = "$message;
+git add ./;
+git commit -m $message;
+git push
